050ef8524aa8173436377a5c57a8d9c3e7273e2a
mahmoudbib <mahm1162003@gmail.com>
2026-09-23 01:44:02 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID'
+};
+// اكتب رقم واتساب بصيغة دولية من غير + أو مسافات. مثال مصر: 2010xxxxxxxx
+export const whatsappNumber = '2010XXXXXXXX';
